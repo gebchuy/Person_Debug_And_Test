d9baf76c3764e9f0dcab8809a36233c6d1e8de04
gebchuy <gebchuy@gmail.com>
2020-09-12 21:50:27 -0300

    Added tests

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,16 +6,56 @@ namespace Tests
 {
     public class Tests
     {
+        Person person1;
+
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
+            person1 = new Person("Gonzalo", "4.877.368-7");
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void NameCorrectlyAssigned()
         {
-            // Insertá tu código  de pruebaaquí
+            string expected = "Gonzalo";
+            string actual = person1.Name;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void IDCorrectlyAssigned ()
+        {
+            string expected = "4.877.368-7";
+            string actual = person1.ID;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void ValidNameAssigned()
+        {
+            person1.Name = "Guzman";
+            Assert.AreEqual("Guzman",person1.Name);
+        }
+
+        [Test]
+        public void NameNotValid()
+        {
+            person1.Name = "";
+            Assert.AreEqual("Gonzalo",person1.Name);
+        }
+
+        [Test]
+        public void validIDAssigned()
+        {
+            person1.ID = "4.579.395-3";
+            Assert.AreEqual("4.579.395-3",person1.ID);
+        }
+        
+        [Test]
+        public void IDNotValid()
+        {
+            person1.ID = "4.877.368-1";
+            Assert.AreEqual("4.877.368-7",person1.ID);
         }
     }
 }
